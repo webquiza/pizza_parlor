7ed1cea4124e04082c9013c1d27a4e050a46af96
Carlos Urquiza <webquiza@gmail.com>
2021-03-03 12:21:47 -0800

    add empty js contructor/prototype and jQuery .ready function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 // Business logic
 
+function Pizza() {
+  
+}
 
-
-
-
+Pizza.prototype.pizzaPrice = function() {
+ 
+}
 
 // User interface logic 
+
+$( document ).ready(function() {
+  $("form#pizza_order").submit(function(event) {
+    event.preventDefault();
+
+  });
+});
