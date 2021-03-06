ff282781992037e8acebfd37f4637a9431d3f044
Carlos Urquiza <webquiza@gmail.com>
2021-03-05 22:10:52 -0800

    adds a working Pizza constructor for toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 // Business logic
 
-function Pizza() {
-  
+function Pizza(toppings) {
+  this.toppings = toppings;
+  this.price = 0;
 }
 
-Pizza.prototype.pizzaPrice = function() {
- 
+Pizza.prototype.chosenToppings = function() {
+  this.price = this.price + (this.toppings.length * 1);
+  return this.price;
 }
 
 // User interface logic 
@@ -14,7 +16,18 @@ $( document ).ready(function() {
   $("form#pizza_order").submit(function(event) {
     event.preventDefault();
 
+  let toppingSelected = $("input:checkbox[name=toppings]:checked").map(function() {
+    return this.value;
+  });
+
+  let guestPizza = new Pizza(toppingSelected);
+
+  guestPizza.chosenToppings();
+
+  const answer = guestPizza.price;
+
   $("#answer").show();
+  $("#output").text(answer)
 
   });
 });
